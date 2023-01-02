68e97a4682ef48909b6b109faa77397f91219571
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-02 17:49:04 -0500

    just finished basic set up

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    // [port] what port on our local machine to run the dev server
+    port: 3000,
+  }
+}
