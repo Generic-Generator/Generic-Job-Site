499dd5dbd2f99eead5dda591bb2b7cd18e1bec8d
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-06 17:12:26 -0500

    test of fake log in complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Jobs from './Jobs.jsx';
 
 function App() {
   const [user, setUser] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const updateUser = (e) => {
     setUser(e.target.value);
@@ -10,13 +11,18 @@ function App() {
 
   const loggingIn = () => {
     console.log(user)
+    setLoggedIn(!loggedIn)
   }
 
   return (
     <div>
-      <h1>Job Finder</h1>
-      <input type="text" placeholder="temp username field" onChange={updateUser}></input>
-      <button onClick={loggingIn}>sketch what I can first</button>
+      {!loggedIn && <div>
+        <h1>Job Finder</h1>
+        <input type="text" placeholder="temp username field" onChange={updateUser}></input>
+        <button onClick={loggingIn}>sketch what I can first</button>
+
+      </div>}
+      {loggedIn && <h1>{`${user} logged in`}</h1>}
     </div>
   )
 }
