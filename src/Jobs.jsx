e34d36ef5252b5e8af263499ea141c801d8cc280
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-11 12:15:05 -0500

    set up search functionality

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -5,6 +5,9 @@ import JobsApplied from './JobsApplied.jsx';
 function Jobs({jobs, user}) {
   const [appliedFor, setAppliedFor] = useState([])
   const [showApplied, setShowApplied] = useState(false);
+  const [searching, setSearching] = useState(false);
+  const [searched, setSearched] = useState('');
+  const [filtered, setfiltered] = useState([]);
 
   const displayApplied = () => {
     setShowApplied(!showApplied)
@@ -16,12 +19,23 @@ function Jobs({jobs, user}) {
       {showApplied && <h1>{`Jobs user ${user} Applied for`}</h1>}
       {!showApplied && <button onClick={displayApplied}>view Jobs Applied to</button>}
       {showApplied && <button onClick={displayApplied}>view Job Openings</button>}
+      {!showApplied &&
+      <div>
+        <br/>
+        <input type='text' placeholder='Search for Jobs'></input>
+        </div>}
       {showApplied && <JobsApplied applied={appliedFor}/>}
-      {!showApplied && jobs.map((job, i) => {
+      {!showApplied && !searching && jobs.map((job, i) => {
         return (
         <JobPosting key={i} job={job} applied={appliedFor} addApplied={(job) => {setAppliedFor(appliedFor.concat([job]))}} />
         )
         })}
+        {!showApplied && searching && filtered.length > 0 && filtered.map((job, i) => {
+        return (
+        <JobPosting key={i} job={job} applied={appliedFor} addApplied={(job) => {setAppliedFor(appliedFor.concat([job]))}} />
+        )
+        })}
+        {!showApplied && searching && filtered.length === 0 && <div>No Results Matching Your Search</div>}
     </div>
   )
 }
