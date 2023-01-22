98e1ca16ee0e1bf087e151bbca1a8ebc94c7e611
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-21 19:48:57 -0500

    clearin button clears applied history

--- src/JobsApplied.jsx
+++ src/JobsApplied.jsx
@@ -1,7 +1,18 @@
 import React, {useState} from 'react';
 import Accordion from './accordion.js';
+import axios from 'axios';
 
-function JobsApplied({applied}) {
+function JobsApplied({applied, user}) {
+
+  const clearApplied = () => {
+    axios.delete(`/clear/${user}`)
+    .then((res) => {
+      console.log('should be cleared, then add setapplied function')
+    })
+    .catch((err) => {
+      console.log('error clearing applied history')
+    })
+  }
 
   return (
     <div>
@@ -15,6 +26,7 @@ function JobsApplied({applied}) {
         ind={i}
       />)
     })}
+    {applied.length > 0 && <div><button onClick={clearApplied} className='warning'>Warning: UnApply to All Jobs!!!</button></div>}
     {applied.length === 0 && <div>It Does Not Appear You Have Applied To Any Jobs This Session</div>}
     </div>
   )
