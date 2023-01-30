bcc1dbbecf782be9d1e71051dd1835f4848a0f1b
Andy Simmons <rocknswimmer116@gmail.com>
2023-01-29 20:55:42 -0500

    fake modal displaying

--- src/Poster.jsx
+++ src/Poster.jsx
@@ -3,15 +3,21 @@ import Modal from './modal.js';
 import Accordion from './accordion.js';
 
 function Poster({poster}) {
+  const [addPosting, setAddPosting] = useState(false)
+
+  const createJob = () => {
+    setAddPosting(!addPosting)
+  }
 
   return (
     <div>
     <h1>{`Welcome Poster ${poster}`}</h1>
-    <button>post a new job</button>
+    <button onClick={createJob}>post a new job</button>
     <h2>Created Jobs</h2>
     {[11,12,13,14].map((x,y,z) => {
       return (<div>{z[y]}</div>)
     })}
+    {addPosting && <div>modal will go here</div>}
     </div>
   )
 }
