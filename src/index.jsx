b2218e0a6fe90dba9b763907049054c5969d52b4
Andy Simmons <rocknswimmer116@gmail.com>
2023-09-27 13:35:25 -0400

    first steps to making this a multi page app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,28 @@
-import React, {lazy} from 'react';
-import { createRoot } from 'react-dom/client';
-const App = lazy(() => import('./App.jsx'));
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+// import React, {lazy} from 'react';
+// import { createRoot } from 'react-dom/client';
+// const App = lazy(() => import('./App.jsx'));
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(<App />);
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
